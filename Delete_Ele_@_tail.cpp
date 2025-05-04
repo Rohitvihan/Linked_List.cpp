14f5e356006f59805823fb05900a9a07c387289c
Rohit vihan k <167102294+Rohitvihan@users.noreply.github.com>
2025-05-04 13:31:17 +0530

    Add files via upload

---
+++ Delete_Ele_@_tail.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int data1,Node* next1)
+    {
+        data=data1;
+        next=next1;
+    }
+    Node(int data1)
+    {
+        data=data1;
+        next=nullptr;
+    }
+};
+Node* conv_arr2LL(vector<int>&arr)
+{
+    Node* head=new Node(arr[0]);
+    Node*mover=head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node*temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+void printlist(Node* head)
+{
+    Node*cur=head;
+    while(cur)
+    {
+        cout<<cur->data<<" ";
+        cur=cur->next;
+    }
+}
+Node* dele_at_Head(Node* head)
+{
+    if(head==NULL)
+    {
+        return 0;
+    }
+    Node* temp=head;
+    head=head->next;
+    delete temp;
+    return head;
+}
+Node* dele_at_tail(Node* head)
+{
+    if(head==NULL||head->next==NULL)
+    {
+        return NULL;
+    }
+    Node* temp=head;
+    while(temp->next->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    delete temp->next->next;
+    temp->next=nullptr;
+    return head;
+}
+int main()
+{
+    vector<int> arr={15,23,45,75,89};
+    Node* res=conv_arr2LL(arr);
+    Node* tail=dele_at_tail(res);
+    printlist(tail);
+    return 0;
+}
