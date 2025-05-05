5282635c90f27edc45c1490ff353c99c48e5049a
Rohit vihan k <167102294+Rohitvihan@users.noreply.github.com>
2025-05-05 08:13:42 +0530

    Add files via upload

---
+++ Converting_arr_to_DLL.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    Node* backk;
+    public:
+    Node(int data1,Node* backk1,Node* next1)
+    {
+        data=data1;
+        backk=backk1;
+        next=next1;
+    }
+    public:
+    Node(int data1)
+    {
+        data=data1;
+        backk=nullptr;
+        next=nullptr;
+    }
+};
+Node* Conv_arr2DLL(vector<int> &arr)
+{
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node* temp=new Node(arr[i],prev,nullptr);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+void printlist(Node* head)
+{
+    Node* cur=head;
+    while(cur)
+    {
+        cout<<cur->data<<" ";
+        cur=cur->next;
+    }
+}
+int main()
+{
+    vector<int> arr={1,3,2,4,5};
+    Node* ans=Conv_arr2DLL(arr);
+    cout<<ans->data<<endl;
+    printlist(ans);
+    return 0;
+}
