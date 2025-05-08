81f74834e562578cbcca9ca557205af00ae96eb8
Rohit vihan k <167102294+Rohitvihan@users.noreply.github.com>
2025-05-08 07:16:55 +0530

    Add files via upload

---
+++ Insert_Before_head_DLL.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    Node* backk;
+    public:
+    Node(int data1,Node* backk1,Node* next1)
+    {
+        data=data1;
+        backk=backk1;
+        next=next1;
+    }
+    public:
+    Node(int data1)
+    {
+        data=data1;
+        backk=nullptr;
+        next=nullptr;
+    }
+};
+Node* Conv_arr2DLL(vector<int> &arr)
+{
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node* temp=new Node(arr[i],prev,nullptr);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+void printlist(Node* head)
+{
+    Node* cur=head;
+    while(cur)
+    {
+        cout<<cur->data<<" ";
+        cur=cur->next;
+    }
+}
+Node* Insert_before_head(Node* head,int val)
+{
+    Node* newNode=new Node(val,nullptr,head);
+    head->backk=newNode;
+    head=newNode;
+    return head;
+}
+int main()
+{
+    vector<int> arr={1,3,2,4,5};
+    Node* ans=Conv_arr2DLL(arr);
+    int val=10;
+    Node* ans1=Insert_before_head(ans,val);
+    printlist(ans1);
+    return 0;
+}
